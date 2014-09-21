1ef39cff9f91448c211a77b7f6304b13f402704e
tart2000 <tart2000design@gmail.com>
2014-09-20 23:53:54 -0400

    edition added to project submit form

--- client/views/projects/project_submit.js
+++ client/views/projects/project_submit.js
@@ -1,11 +1,12 @@
 Template.projectSubmit.events({ 
     'submit': function(e) {
-        console.log(e);
         e.preventDefault();
     
     var project = {
         title: $(e.target).find('[id=title]').val(), 
-        baseline: $(e.target).find('[id=baseline]').val()
+        baseline: $(e.target).find('[id=baseline]').val(),
+        edition: $(e.target).find('[id=edition]').val(),
+        team: []
     }
 
     project._id = Projects.insert(project);
--- server/fixtures.js
+++ server/fixtures.js
@@ -4,24 +4,28 @@ if (Projects.find().count() === 0) {
         title: 'Telescope',
         baseline:'something awesome',
         author: 'Sacha Grief',
+        edition:'Montreal',
     }); 
 
     Projects.insert({ 
         title: 'Meteor',
         baseline : 'building awesome apps easily',
         author: 'Tom Coleman',
+        edition:'Nantes',
     }); 
 
     Projects.insert({ 
         title: 'PingPong',
         baseline : 'a one-way trip to funky town',
         author: 'Tom Coleman',
+        edition:'Leman',
     }); 
 
     Projects.insert({ 
         title: 'The Meteor Book',
-        baseline : 'Defenitely something worse looking into!',
-        author: 'Tom Coleman',
+        baseline : 'Defenitely something worth looking into!',
+        author: 'John Good',
+        edition:'Leman',
     }); 
 
 
