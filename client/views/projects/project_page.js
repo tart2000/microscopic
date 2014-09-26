5fed0d06c6a4e184ea9e451241c5aa9e39ae0529
tart2000 <tart2000design@gmail.com>
2014-09-26 14:14:26 -0400

    Comments made better (again)
    order is inverted and all

--- client/views/projects/project_page.js
+++ client/views/projects/project_page.js
@@ -9,7 +9,7 @@ Template.projectPage.helpers({
         return Projects.findOne(this._id).url;
     },
     projectComments: function() {
-        return Comments.find({projectId: this._id}); 
+        return Comments.find({projectId: this._id}, {sort: {submitted: -1}}); 
     },
     projectCommentCount : function() {
         var comCount = Comments.find({projectId: this._id}).count(); 
