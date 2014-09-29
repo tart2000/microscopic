4a54d4616b5ff5d2b4e262e55b33a01f7a2f4429
tart2000 <tart2000design@gmail.com>
2014-09-29 17:07:49 -0400

    test

--- client/views/projects/project_edit.js
+++ client/views/projects/project_edit.js
@@ -1,3 +1,4 @@
+
 Template.projectEdit.events({ 
     'submit': function(e) {
         e.preventDefault();
@@ -17,7 +18,8 @@ Template.projectEdit.events({
                 Alert.add('your project has been edited', 'success');
             }
         }); },
-    'click .delete': function(e) { e.preventDefault();
+    'click .delete': function(e) { 
+        e.preventDefault();
         if (confirm("Delete this project?")) {
             var currentProjectId = this._id;
             Projects.remove(currentProjectId);
@@ -30,10 +32,19 @@ Template.projectEdit.events({
         Router.go('projectPage', {_id: this._id});
     },
     'click .add-tag': function(e) {
+        e.preventDefault();
         var currentProjectId = this._id;
         var new_tag = $(e.target).parent().find('[id=tagbox]').val();
-        Projects.update({_id:currentProjectId}, {$push: {tags: new_tag}});
+        Projects.update({_id:currentProjectId}, {$addToSet: {tags: new_tag}});
         $('#tagbox').val('');
+    }, 
+    'click .remove-tag': function(e) {
+        e.preventDefault();
+        var currentProjectId = this._id;
+        var old_tag = $(e.target).parent().parent().text();
+        Projects.update({_id:currentProjectId}, {$pull: {tags: old_tag}});
+        console.log(old_tag);
+        console.log(currentProjectId);
     }
 });
 
