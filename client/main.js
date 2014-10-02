c1bd1cb610b849a7ffde02fc79ac3b10e5307ef9
tart2000 <tart2000design@gmail.com>
2014-10-02 15:34:39 -0400

    link to profile page seems to be working!

--- client/main.js
+++ client/main.js
@@ -4,3 +4,14 @@ Meteor.subscribe('licences');
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
+
+Template._loginButtonsLoggedInDropdown.rendered = function (){
+    $( "#login-buttons-open-change-password" ).before( "<a href='#' id='profileLink' class='btn btn-default btn-block'>Your profile</a>" );
+};
+Template._loginButtonsLoggedInDropdown.events({
+    'click #profileLink': function(event) {
+        event.stopPropagation();
+        Template._loginButtons.toggleDropdown();
+        Router.go('userProfile', {_id: Meteor.userId});
+    }
+});
