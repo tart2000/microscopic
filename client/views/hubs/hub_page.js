119aaf9488789a49501d79debdd75f0469f691d5
tart2000 <tart2000design@gmail.com>
2014-10-03 11:01:53 -0400

    hub users added on hub page

--- client/views/hubs/hub_page.js
+++ client/views/hubs/hub_page.js
@@ -1,5 +1,8 @@
 Template.hubPage.helpers({ 
     hubProjects: function() {
         return Projects.find({hub: this.name}); 
+    }, 
+    hubUsers: function() {
+        return Meteor.users.find({'profile.hub':this._id});
     }
 });
