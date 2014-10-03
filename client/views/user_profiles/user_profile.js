04a7cdabb9c2f45eed680a2a5c3ac920ad97fe80
gcool-info <george.koulouris1@gmail.com>
2014-10-03 13:45:52 +0100

    Info appearing on user profile is drawn from the db

--- client/views/user_profiles/user_profile.js
+++ client/views/user_profiles/user_profile.js
@@ -1,5 +1,35 @@
 Template.userProfile.helpers({ 
 	getUserPhoto: function() {
 		return userPhotos.findOne(this.profile.thumblink).url();
+	},
+	getHub: function() {
+		return Hubs.findOne(this.profile.hub).name;
+	},
+	getSocial: function(medium) {
+		var socialLink = null;
+		switch(medium) {
+            case 'facebook':
+                socialLink = this.profile.social.facebook;
+                break;
+            case 'twitter':
+                socialLink = this.profile.social.twitter;
+                break;
+            case 'linkedin':
+                socialLink = this.profile.social.linkedin;
+                break;
+            case 'instagram':
+                socialLink = this.profile.social.instagram;
+                break;
+            case 'tumblr':
+                socialLink = this.profile.social.tumblr;
+                break;
+            case 'website':
+                socialLink = this.profile.social.website;
+                break;
+        }
+        if (socialLink)
+        	return socialLink;
+        else
+        	return false;
 	}
 });
